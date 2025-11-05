786a56f5f5a6df4ca4a26e7cdcd4fd96bd199e42
serchonodera117 <sergio.lobo117@gmail.com>
2025-11-05 00:28:19 -0600

    Update: quick sort explanation

--- sorting_algorithms/generic_explaination.js
+++ sorting_algorithms/generic_explaination.js
@@ -15,8 +15,26 @@ let bubble_sort = (arr)=>{
 
 
 //||||||||||||||||||||||||||||||||QUICK SORT||||||||||||||||||||||||||||||||||||||
-let quick_sort = (arr)=>{
-    return arr;
+let partition = (arr, low, high)=>{
+    let pivot = arr[high];
+    let i  = low-1
+    
+    for(let j = low; j<=high-1; j++){
+        if(arr[j]< pivot){ //if current value is smaller than pivot, increment index of smaller [0,2,3,4,60,10, 8]
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]] ///swap smaller elements 
+        }
+    }
+    [arr[i+1], arr[high]] = [arr[high], arr[i+1]] //swap pivot.
+    
+    return i+1;
+}
+
+function QuickSort(arr, low, high){
+    if(low >= high) return;
+    let pivot  = partition(arr, low, high);
+    QuickSort(arr, low, pivot-1); //left: low to pivot (pi-1 because pivot stay statick) 
+    QuickSort(arr, pivot+1, high);  //right: pivot to high (pivot +1 because pivot stay statick)
 }
 
 
@@ -44,12 +62,19 @@ function randomlyFill(number){
 function tets(amount, sortMethod){
     console.log("\n-----TESTS-----\n")
     for(let i = 0; i< amount; i++){
-        console.log(`Sorted Test ${i}: `, sortMethod(array_test[i]));
+        // console.log(`Sorted Test ${i}: `, sortMethod(array_test[i])); //bubble sort parameters
+        console.log(`Sorted Test ${i}: `, sortMethod(array_test[i])); //quick sort parameters
     }
 }
 
 
-console.log("---- BubbleSort: ---- \n");
 randomlyFill(3);
-console.log(bubble_sort(array_test[0]));
-tets(3, bubble_sort);
+
+// console.log("---- BubbleSort: ---- \n");
+// console.log(bubble_sort(array_test[0]));
+// tets(3, bubble_sort);
+
+console.log("---- QuickSort: ---- \n");
+let a  =array_test[0]
+QuickSort(a, 0, a.length-1)
+console.log("Array 1:", a);
